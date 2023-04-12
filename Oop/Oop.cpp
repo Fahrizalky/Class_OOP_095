37dc6c3af4ae6d6f3691cdf0c49fda0b00a849f2
Fahrizalky <fahrizalkusumayuda2003@gmail.com>
2023-04-12 10:52:40 +0700

    void input perintah public

--- Oop/Oop.cpp
+++ Oop/Oop.cpp
@@ -6,4 +6,13 @@ private:
 	int nim;
 	string nama;
 	float nilai;
+public:
+	void input() {
+		cout << "Masukan NIM : ";
+		cin >> nim;
+		cout << "Masukan nama : ";
+		cin >> nama;
+		cout << "Masukan nilai : ";
+		cin >> nilai;
+	}
 
